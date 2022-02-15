04d29908b3bec65bc54fb4e8d99dc14fa2832f5f
lesrz <suarezarnaldi.leo@gmail.com>
2022-02-14 23:38:37 -0300

    Update brand name and add divider to cards

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,26 @@ import logo from './logo.svg';
 import './App.css';
 import data from './data/data';
 import ActionCard from './components/ActionCard';
+import { Divider } from '@mui/material';
+import styles from './components/styles';
 
 function App() {
   const cards: {}[] = data.map((info: any) => {
-    return <ActionCard key={info.id} info={info} />;
+    return (
+      <div>
+        <ActionCard key={info.id} info={info} />
+        <Divider light={true} sx={styles.divider} />
+      </div>
+    );
   });
 
   return (
     <div className='App'>
       <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p className='App-title'>tournal</p>
+        <a href='./' className='App-title'>
+          <img src={logo} className='App-logo' alt='logo' />
+          tournal
+        </a>
       </header>
       <section className='cards--list'>{cards}</section>
     </div>
